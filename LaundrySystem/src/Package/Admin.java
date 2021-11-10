273d327a98cfb8e37c0991d505287fdef9d24694
Shuruq Baabdullah <sh-oo@live.com>
2021-11-11 00:30:15 +0300

    search for order in class admin is fixed and done coding

--- LaundrySystem/src/Package/Admin.java
+++ LaundrySystem/src/Package/Admin.java
@@ -23,14 +23,36 @@ public class Admin extends Person {
 
     }
 
-    public boolean searchOrder() {
-        return true;
+    public void searchForOrder(int idOrder) {
+        
+        boolean foundFlag=false;
+        for (int i = 0; i < orders.size(); i++) {
+            if (idOrder == orders.get(i).getOrderID()) {
+                System.out.println("The order is exist in the system!");
+                System.out.println("Here is the order information: ");
+                System.out.println("Customer information: ");
+                System.out.println("The customer name: " + orders.get(i).getCustomer().getFirstName() +" "+
+                orders.get(i).getCustomer().getLastName());
+                System.out.println("The customer name: " + orders.get(i).getCustomer().getPhoneNumber());
+                System.out.println("The customer name: " + orders.get(i).getCustomer().getEmail());
+                System.out.println("Order information: ");
+                System.out.println("The order ID: " + orders.get(i).getOrderID());
+                System.out.println("The order status: " + orders.get(i).getStatus());
+                System.out.println("The service type: " + orders.get(i).getService().getTypeOfService());
+                System.out.println("The date of order: " + orders.get(i).getDateOfOrder());
+                System.out.println("The date of delivery: " + orders.get(i).getDateOfDelivery());
+                System.out.println("The total price is: " + orders.get(i).getTotalPrice());
+                foundFlag=true;
+                break;
+            }
+        }
+        if (!foundFlag) {
+            System.out.println("Sorry the order with "+ idOrder + " is not in the system."); 
+        }
     }
 
     public void getCustomerOrders() {
 
     }
-
-  
 
 }
