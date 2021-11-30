a361541a83c70a895b394f0b9f03f2414c35a5f2
Ghaliah <61186194+Frostie7@users.noreply.github.com>
2021-11-30 18:22:22 +0300

    Update StartUpPage.java

--- LaundrySystem/src/Frames/StartUpPage.java
+++ LaundrySystem/src/Frames/StartUpPage.java
@@ -57,7 +57,7 @@ public class StartUpPage extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         this.dispose();
-        new LoginPage().setVisible(true);
+        new LoginFrame().setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
